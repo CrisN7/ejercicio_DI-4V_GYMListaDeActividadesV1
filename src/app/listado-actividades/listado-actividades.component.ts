47d4f9c1314f6b1ef9169ed968641eff37c343b6
Cristian Rojas <cris_2110@outlook.com>
2024-11-28 11:13:28 +0100

    Generé los componentes navbar, lista de actividades, y actividad

---
+++ src/app/listado-actividades/listado-actividades.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-listado-actividades',
+  imports: [],
+  templateUrl: './listado-actividades.component.html',
+  styleUrl: './listado-actividades.component.scss'
+})
+export class ListadoActividadesComponent {
+
+}
